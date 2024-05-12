1d4b6c4affa1f2bc7a35d9471a8f88f453a05952
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-05-12 23:41:36 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,21 +1,21 @@
-CREATE DATABASE [db_3.05]
+CREATE DATABASE [db_3.05];
 GO
-USE [db_3.05]
+USE [db_3.05];
 GO
 
 CREATE TABLE Suppliers (
     SupplierID INT PRIMARY KEY IDENTITY(1,1),
-    SupplierName VARCHAR(255),
-    SupplierLocation VARCHAR(255)
+    SupplierName VARCHAR(255) NOT NULL,
+    SupplierLocation VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Products (
     ProductID INT PRIMARY KEY IDENTITY(1,1),
-    ProductName VARCHAR(255),
-    ProductType VARCHAR(100),
-    SupplierID INT,
-    Quantity INT,
-    CostPrice DECIMAL(10, 2),
-    DeliveryDate DATE,
+    ProductName VARCHAR(255) NOT NULL,
+    ProductType VARCHAR(100) NOT NULL,
+    SupplierID INT NOT NULL,
+    Quantity INT NOT NULL CHECK (Quantity >= 0), 
+    CostPrice DECIMAL(10, 2) NOT NULL CHECK (CostPrice >= 0.00),  -- Ensures CostPrice is never negative
+    DeliveryDate DATE NOT NULL,
     FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID)
 );
